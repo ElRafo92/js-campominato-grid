7e396f8eaaf6015848c16b07737165a06fe7e093
ElRafo92 <rafael.martinez@outlook.it>
2022-11-02 21:21:12 +0100

    creato il cambio colore con un click

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
+'use strict'
 const genera = document.querySelector('.genera');
 let tabella = document.querySelector('.tabella')
 
 genera.addEventListener('click', function() {
-    for (i = 1; i < 101; i++) {
-    let casella = `<div class="casella bg-green">${i}</div>`;
-    tabella.innerHTML += casella;
-    }
+    for (let i = 1; i < 11; i++) {
+    const casella = document.createElement('div');
+    casella.classList.add('casella', 'bg-green');
+    casella.innerHTML = i;
+
+    casella.addEventListener('click', function() {
+        casella.classList.remove('bg-green');
+        casella.classList.add('bg-red');
+    })
     
+    tabella.append(casella);
+    };
 });
 
