0a0c73fb0a4306400294085de0d0198b3ce3ebc0
ElRafo92 <rafael.martinez@outlook.it>
2022-11-02 21:54:25 +0100

    aggiunto bonus

--- js/main.js
+++ js/main.js
@@ -3,16 +3,27 @@ const genera = document.querySelector('.genera');
 let tabella = document.querySelector('.tabella')
 
 genera.addEventListener('click', function() {
-    for (let i = 1; i < 11; i++) {
+    let difficolta = document.getElementById('difficolta').value;
+    console.log(difficolta)
+
+    for (let i = 1; i <= difficolta; i++) {
     const casella = document.createElement('div');
     casella.classList.add('casella', 'bg-green');
     casella.innerHTML = i;
+
+    if (difficolta == '100') {
+        casella.classList.add('casella-width1')
+    } else if (difficolta == '81') {
+        casella.classList.add('casella-width2')
+    } else {
+        casella.classList.add('casella-width3')
+    }
 
     casella.addEventListener('click', function() {
         casella.classList.remove('bg-green');
         casella.classList.add('bg-red');
     })
-    
+
     tabella.append(casella);
     };
 });
